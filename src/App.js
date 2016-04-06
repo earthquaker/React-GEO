36c95e33a9d5135ad6f768c8383f6a5abbe9c026
Elias Hansen <pe_lias@msn.com>
2016-04-06 20:17:04 +0200

    got it to work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import Geosuggest from 'react-geosuggest';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onSuggestSelect = this.onSuggestSelect.bind(this);
+  }
+
+  onSuggestSelect(suggest) {
+    console.log(suggest);
+  }
+
   render() {
+    let fixtures = [
+      {label: 'Old Elbe Tunnel, Hamburg', location: {lat: 53.5459, lng: 9.966576}},
+      {label: 'Reeperbahn, Hamburg', location: {lat: 53.5495629, lng: 9.9625838}},
+      {label: 'Alster, Hamburg', location: {lat: 53.5610398, lng: 10.0259135}}
+    ];
+
     return (
-      <h1>Hello, world.</h1>
+      <Geosuggest
+        placeholder="Start typing!"
+        initialValue="Hamburg"
+        fixtures={fixtures}
+        onSuggestSelect={this.onSuggestSelect}
+        location={new google.maps.LatLng(53.558572, 9.9278215)}
+        radius="20" />
     );
   }
+
 }
